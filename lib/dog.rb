73ae899dbac5f73bd258db55b30ad07555c5980c
Tavis Weeden <tavisweeden@gmail.com>
2019-11-08 02:20:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+class Dog 
+  
+  def name=(dogs_name)
+    @dogs_name_is = dogs_name 
+    
+  end
+  
+  def name 
+    @dogs_name_is 
+  end
+
+def bark
+    puts "woof!"
+  end
+end
+
